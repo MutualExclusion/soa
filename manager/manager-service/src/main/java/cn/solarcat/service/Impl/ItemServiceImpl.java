1718c31df3692ecde21832e07168e74d60bd9b06
MutualExclusion <m18845636132@163.com>
2019-01-23 14:46:06 +0800

    更新工程

--- manager/manager-service/src/main/java/cn/solarcat/service/Impl/ItemServiceImpl.java
+++ manager/manager-service/src/main/java/cn/solarcat/service/Impl/ItemServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
@@ -12,9 +11,8 @@ import javax.jms.TextMessage;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
@@ -45,11 +43,74 @@ public class ItemServiceImpl implements ItemService {
 	private RedisTemplate<String, String> redisTemplate;
 	private String REDIS_ITEM_PRE = "ITEM_INFO";
 	private Integer ITEM_CACHE_EXPIRE = 3600;
-	@Qualifier(value = "activeMQTopic")
 	@Autowired
-	private Destination topic;
-	@Autowired
-	private JmsMessagingTemplate jmsMessagingTemplate;
+	private JmsTemplate jmsTemplate;
+
+	@Override
+	public SolarCatResult addItem(TbItem tbItem, String desc) {
+		final long Id = IDUtils.genItemId();
+		tbItem.setId(Id);
+		tbItem.setStatus((byte) 1);
+		tbItem.setCreated(new Date());
+		tbItem.setUpdated(new Date());
+		itemMapper.insert(tbItem);
+		TbItemDesc tbItemDesc = new TbItemDesc();
+		tbItemDesc.setItemId(Id);
+		tbItemDesc.setItemDesc(desc);
+		tbItemDesc.setCreated(new Date());
+		tbItemDesc.setUpdated(new Date());
+		itemDescMapper.insert(tbItemDesc);
+		// 发送一个商品添加消息
+//		jmsTemplate.convertAndSend(itemTopic, new MessageCreator() {
+//			@Override
+//			public Message createMessage(Session session) throws JMSException {
+//				TextMessage textMessage = session.createTextMessage();
+//				return textMessage;
+//			}
+//		});
+		jmsTemplate.send("ITEM_ADD_TOPIC", new MessageCreator() {
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				TextMessage textMessage = session.createTextMessage();
+				return textMessage;
+			}
+		});
+		// jmsTemplate.convertAndSend(itemTopic, jsonObject);
+		return SolarCatResult.ok();
+	}
+
+	@Override
+	public SolarCatResult deleteItem(Long itemId) {
+		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+		tbItem.setStatus((byte) 3);
+		itemMapper.updateByPrimaryKey(tbItem);
+		return SolarCatResult.ok();
+	}
+
+	@Override
+	public TbItem getItem(Long itemId) {
+		TbItem tbItem = new TbItem();
+		tbItem = itemMapper.selectByPrimaryKey(itemId);
+		return tbItem;
+	}
+
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		// 设置分页信息
+		PageHelper.startPage(page, rows);
+		// 执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		// 创建一个返回值对象
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setRows(list);
+		// 取分页结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		// 取总记录数
+		long total = pageInfo.getTotal();
+		result.setTotal((int) total);
+		return result;
+	}
 
 	@Override
 	public TbItem getTbItemById(long itemId) {
@@ -81,96 +142,6 @@ public class ItemServiceImpl implements ItemService {
 	}
 
 	@Override
-	public EasyUIDataGridResult getItemList(int page, int rows) {
-		// 设置分页信息
-		PageHelper.startPage(page, rows);
-		// 执行查询
-		TbItemExample example = new TbItemExample();
-		List<TbItem> list = itemMapper.selectByExample(example);
-		// 创建一个返回值对象
-		EasyUIDataGridResult result = new EasyUIDataGridResult();
-		result.setRows(list);
-		// 取分页结果
-		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
-		// 取总记录数
-		long total = pageInfo.getTotal();
-		result.setTotal((int) total);
-		return result;
-	}
-
-	@Override
-	public SolarCatResult addItem(TbItem tbItem, String desc) {
-		final long Id = IDUtils.genItemId();
-		tbItem.setId(Id);
-		tbItem.setStatus((byte) 1);
-		tbItem.setCreated(new Date());
-		tbItem.setUpdated(new Date());
-		itemMapper.insert(tbItem);
-		TbItemDesc tbItemDesc = new TbItemDesc();
-		tbItemDesc.setItemId(Id);
-		tbItemDesc.setItemDesc(desc);
-		tbItemDesc.setCreated(new Date());
-		tbItemDesc.setUpdated(new Date());
-		itemDescMapper.insert(tbItemDesc);
-		// 发送一个商品添加消息
-		jmsMessagingTemplate.convertAndSend(topic, new MessageCreator() {
-			@Override
-			public Message createMessage(Session session) throws JMSException {
-				TextMessage textMessage = session.createTextMessage(Id + "");
-				return textMessage;
-			}
-		});
-//		jmsTemplate.convertAndSend(topic, new MessageCreator() {
-//			@Override
-//			public Message createMessage(Session session) throws JMSException {
-//				TextMessage textMessage = session.createTextMessage(Id + "");
-//				return textMessage;
-//			}
-//		});
-		return SolarCatResult.ok();
-	}
-
-	@Override
-	public SolarCatResult deleteItem(Long itemId) {
-		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
-		tbItem.setStatus((byte) 3);
-		itemMapper.updateByPrimaryKey(tbItem);
-		return SolarCatResult.ok();
-	}
-
-	@Override
-	public TbItem getItem(Long itemId) {
-		TbItem tbItem = new TbItem();
-		tbItem = itemMapper.selectByPrimaryKey(itemId);
-		return tbItem;
-	}
-
-	@Override
-	public SolarCatResult updateItemAndDesc(TbItem item, TbItemDesc itemDesc) {
-		item.setUpdated(new Date());
-		itemMapper.updateByPrimaryKeySelective(item);
-		itemDesc.setUpdated(new Date());
-		itemDescMapper.updateByPrimaryKeySelective(itemDesc);
-		return SolarCatResult.ok();
-	}
-
-	@Override
-	public SolarCatResult updateInstock(Long itemId) {
-		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
-		tbItem.setStatus((byte) 2);
-		itemMapper.updateByPrimaryKey(tbItem);
-		return SolarCatResult.ok();
-	}
-
-	@Override
-	public SolarCatResult updateReshelf(Long itemId) {
-		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
-		tbItem.setStatus((byte) 1);
-		itemMapper.updateByPrimaryKey(tbItem);
-		return SolarCatResult.ok();
-	}
-
-	@Override
 	public TbItemDesc getTbItemDescById(long itemId) {
 		try {
 			String json = redisTemplate.opsForValue().get(REDIS_ITEM_PRE + ":" + itemId + ":DESC");
@@ -191,6 +162,31 @@ public class ItemServiceImpl implements ItemService {
 			e.printStackTrace();
 		}
 		return tbItemDesc;
+	}
+
+	@Override
+	public SolarCatResult updateInstock(Long itemId) {
+		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+		tbItem.setStatus((byte) 2);
+		itemMapper.updateByPrimaryKey(tbItem);
+		return SolarCatResult.ok();
+	}
+
+	@Override
+	public SolarCatResult updateItemAndDesc(TbItem item, TbItemDesc itemDesc) {
+		item.setUpdated(new Date());
+		itemMapper.updateByPrimaryKeySelective(item);
+		itemDesc.setUpdated(new Date());
+		itemDescMapper.updateByPrimaryKeySelective(itemDesc);
+		return SolarCatResult.ok();
+	}
+
+	@Override
+	public SolarCatResult updateReshelf(Long itemId) {
+		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+		tbItem.setStatus((byte) 1);
+		itemMapper.updateByPrimaryKey(tbItem);
+		return SolarCatResult.ok();
 	}
 
 }
