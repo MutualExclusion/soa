6627dd48dc09838cfddf5abd640583110048f53c
MutualExclusion <m18845636132@163.com>
2019-01-23 14:53:09 +0800

    update

--- search/search-interface/src/main/java/cn/solarcat/search/service/SearchItemService.java
+++
@@ -1,7 +0,0 @@
-package cn.solarcat.search.service;
-
-import cn.solarcat.common.util.SolarCatResult;
-
-public interface SearchItemService {
-	SolarCatResult importAllItems();
-}
--- search/search-interface/src/main/java/cn/solarcat/search/service/SearchService.java
+++
@@ -1,7 +0,0 @@
-package cn.solarcat.search.service;
-
-import cn.solarcat.common.pojo.SearchResult;
-
-public interface SearchService {
-	SearchResult search(String keyword,int page,int rows) throws Exception;
-}
