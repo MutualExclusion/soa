9a68540ef4682a4c21a7eeca74167d6d90ac6ab6
MutualExclusion <m18845636132@163.com>
2019-02-18 15:27:59 +0800

    update

---
+++ sso-web/src/main/java/cn/solarcat/sso/controller/PageController.java
@@ -0,0 +1,12 @@
+package cn.solarcat.sso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	@RequestMapping("/")
+	public String showLogin() {
+		return "login";
+	}
+}
