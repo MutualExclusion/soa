74764e5bd56274ca3f6fbdddc78c418e7ec077f3
MutualExclusion <m18845636132@163.com>
2019-03-01 20:48:30 +0800

    update

--- search-web/src/main/java/cn/solarcat/search/controller/SearchController.java
+++ search-web/src/main/java/cn/solarcat/search/controller/SearchController.java
@@ -1,5 +1,7 @@
 package cn.solarcat.search.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 
-import cn.solarcat.aop.Log;
-import cn.solarcat.common.pojo.ACTION;
-import cn.solarcat.common.pojo.LEVEL;
 import cn.solarcat.common.pojo.SearchResult;
+import cn.solarcat.common.util.JudgeFunction;
 import cn.solarcat.search.service.SearchService;
 
 @Controller
@@ -19,16 +19,19 @@ public class SearchController {
 	private SearchService searchService;
 	private Integer SEARCH_RESULT_ROWS = 32;
 
-	@Log(action = ACTION.MUTUAL, level = LEVEL.CONTROLLER)
 	@RequestMapping("/search")
-	public String searchItemList(String keyword, @RequestParam(defaultValue = "1") Integer page, Model model)
-			throws Exception {
+	public String searchItemList(HttpServletRequest request, String keyword,
+			@RequestParam(defaultValue = "1") Integer page, Model model) throws Exception {
 		SearchResult searchResult = searchService.search(keyword, page, SEARCH_RESULT_ROWS);
 		model.addAttribute("query", keyword);
 		model.addAttribute("totalPages", searchResult.getTotalPages());
 		model.addAttribute("page", page);
 		model.addAttribute("recordCount", searchResult.getRecordCount());
 		model.addAttribute("itemList", searchResult.getItemList());
-		return "search";
+		if (JudgeFunction.JudgeIsMoblie(request)) {
+			return "team";
+		} else {
+			return "search";
+		}
 	}
 }
